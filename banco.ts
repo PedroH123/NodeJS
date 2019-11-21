8746439230e42eb85d7e0b7a74b4bcc7d28c8a93
pedroescola <pedroescola.com@gmail.com>
2019-11-21 17:19:15 +0000

    banco.ts

---
+++ banco.ts
@@ -0,0 +1,41 @@
+namespace empresa{
+    export  class Banco{
+        private _nome:string;
+        private _codBanco:number;
+        private _clientes:Array<Cliente> = [];
+        private _funcionario:Array<Funcionario> = [];
+
+         public getNome(){
+            return this._nome;
+        }
+
+        public setNome(nome:string){
+            this._nome = nome;
+        }
+
+        public getCodBanco(){
+            return this._codBanco;
+        }
+
+        public setCodBanco(codigo:number){
+            this._codBanco = codigo;
+        }
+
+        public getCliente(){
+            return this._clientes;
+        }
+
+        public setCliente(cliente:Cliente){
+            this._clientes.push(cliente);
+        }
+
+         public getFuncionario(){
+            return this._funcionario;
+        }
+
+        public setFuncionario(funcionario:Funcionario){
+            this._funcionario.push(funcionario);
+        }
+
+    }
+}
